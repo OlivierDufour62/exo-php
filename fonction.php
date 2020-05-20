c82fc8b47dad8e8dfbd10f06ac8fcc126dd01aa6
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-05-20 16:22:51 +0200

    createform

--- fonction.php
+++ fonction.php
@@ -1,21 +1,33 @@
 <?php
 
-function number(int $x){
+function number(int $x)
+{
     $i = 0;
     while($i <= $x){
         echo $i++;
     }
 }
 
-function number2(int $x){
+function number2(int $x)
+{
     $i = 0;
     for($i=0; $i<$x;$i++){
         echo $i;
     }
 }
 
-function affInput($x){
+function affInput($x)
+{
     for($i=0; $i<$x;$i++){
         echo '<input type="text">' . '<br>';
     }
+}
+
+function createForm(array $array)
+{
+    
+        foreach($array as $attr){
+            echo '<input name="' . $attr['name'] . '" type="' .$attr['type'].'" class="'.$attr['class'] . '""';
+        }
+    
 }
--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<?php 
+<?php
 
 require('fonction.php');
 
-echo number(10) .'<br>';
+echo number(10) . '<br>';
 
-echo number(10);
+echo number(10)  . '<br>';
 
 echo affInput(5);
 
+$array = array(
+    array('name' => 'nom', 'type' => 'text', 'class' => 'modal'),
+
+    array('name' => 'prenom', 'type' => 'text', 'class' => 'modal'),
+
+    array('name' => 'valider', 'type' => 'submit', 'class' => 'btn')
+);
+?>
+
+<form>
+<?php
+    createForm($array);
+?>
+</form>
